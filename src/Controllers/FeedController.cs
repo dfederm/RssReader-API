b2bfba5fea00718f18e5626b780c75bb30e958df
David Federman <dfederm@microsoft.com>
2016-11-12 19:50:57 -0800

    Add Create action

--- src/Controllers/FeedController.cs
+++ src/Controllers/FeedController.cs
@@ -1,14 +1,31 @@
 namespace RssReader.Controllers0
 {
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
 
     [Route("api/[controller]")]
     public class FeedController : Controller
     {
-        [HttpGet]
-        public string Get()
+        private ILogger logger;
+
+        public FeedController(ILoggerFactory loggerFactory)
         {
-            return "Greetings from .NET Core Web API!";
+            this.logger = loggerFactory.CreateLogger("Feed");
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] string[] feeds)
+        {
+            return this.CreatedAtAction(
+                nameof(this.Get),
+                new { feeds = feeds},
+                feeds);
+        }
+
+        [HttpGet]
+        public string Get([FromQuery] string[] feeds)
+        {
+            return "Greetings from .NET Core Web API! " + feeds.Length + ", " + string.Join(", ", feeds);
         }
     }
 }
